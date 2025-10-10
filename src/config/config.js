2fa6dbda2359328d9cca02dd2326b6d9479144ba
Sundan Patyad <sundan.sharma@psquarecompany.com>
2025-10-10 11:51:18 +0530

    fixed

--- src/config/config.js
+++ src/config/config.js
@@ -2,7 +2,7 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 const config = {
-  port:  9000 ,
+  port:  process.env.PORT ,
   cloudinary: {
     cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
     api_key: process.env.CLOUDINARY_API_KEY,
